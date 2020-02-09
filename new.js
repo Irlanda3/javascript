9b0172def23727006dbc05818d887f549aa1c46a
anayeli ochoa <ochoa@example.com>
2020-02-08 22:44:40 -0700

    learning javascript

---
+++ new.js
@@ -0,0 +1,9 @@
+// Pull request
+// https://github.com/AlanisK/personaswitcher/commit/1188a68ad944c56ad88a98692641fcd829613cf3
+//  esto es para un projecto de Dr. Beaty, donde estamos usando slack.com. github hasta ahora feb 8 2020, 
+//al parecer usaremos javascript y tambien instale nodejs
+//  https://www.youtube.com/watch?v=W6NZfCO5SIk       programmin with mosh
+// control shift p + clone git + js-basics
+//hize un repositorio en github para javascript
+
+
