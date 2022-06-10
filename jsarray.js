839898c4a9627347a57dcf677d49e80d79acbb75
8A <32315516+Irlanda3@users.noreply.github.com>
2022-06-09 23:12:37 -0600

    Add files via upload
    difference between const and let

---
+++ jsarray.js
@@ -0,0 +1,12 @@
+const arr = [5,6];
+arr.push(7);
+console.log(arr); 
+//arr =  10; // error
+//console.log(arr);
+arr[0] = 8;
+console.log(arr);
+
+const addition = (a, b) => a+b;
+const add5 = a => 5+a;
+console.log(addition(6,7));
+console.log(add5(6));
