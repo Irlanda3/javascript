362c00ab16086b2458ea0c41b4037d4e06daec71
8A <32315516+Irlanda3@users.noreply.github.com>
2022-06-29 23:32:51 -0600

    Add files via upload
    arrow fnc

---
+++ arrowFnc.js
@@ -0,0 +1,15 @@
+//Arrow functions were introduced in ES6.
+
+//Arrow functions allow us to write shorter function syntax:
+
+let myFunction = (a, b) => a * b;
+
+hello = function() {
+    return "Hello World!";
+  }
+
+hello2 = () => {
+    return "Hello World!";
+}
+
+console.log("Hello");
