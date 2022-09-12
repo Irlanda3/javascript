e117d6a1a1fce0235fe82c3ef2999eba258a0842
8A <32315516+Irlanda3@users.noreply.github.com>
2022-09-11 23:12:02 -0600

    Add files via upload

---
+++ mathMethods.js
@@ -0,0 +1,6 @@
+// math methods and properties
+console.log(Math.PI); 
+console.log(Math.trunc(Math.PI)); // Returns the integral part of the a numeric expression, x, removing any fractional digits. If x is already an integer, the result is x.
+console.log(Math.round(Math.PI));
+console.log(Math.ceil(Math.PI));
+console.log(Math.floor(Math.PI));
