08e10993d4028a162bc2c739c93311dfa623360b
8A <32315516+Irlanda3@users.noreply.github.com>
2022-09-12 21:48:21 -0600

    Add files via upload

--- mathMethods.js
+++ mathMethods.js
@@ -4,3 +4,22 @@ console.log(Math.trunc(Math.PI)); // Returns the integral part of the a numeric 
 console.log(Math.round(Math.PI));
 console.log(Math.ceil(Math.PI));
 console.log(Math.floor(Math.PI));
+
+console.log(Math.pow(2,10));
+console.log(Math.pow(2,4));
+console.log(Math.pow(5,2));
+
+console.log(Math.min(2, 5, 9));
+console.log(Math.max(2,0.5,9));
+
+console.log(Math.random());//gives anumber between 0 and 1 including 
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+
+// from 1 to 10
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
