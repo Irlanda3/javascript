7895685d610744fdc6986bfbd38584851be01a43
8A <32315516+Irlanda3@users.noreply.github.com>
2022-09-16 22:23:45 -0600

    Add files via upload
    challenge

---
+++ challenge1.js
@@ -0,0 +1,15 @@
+// write code that will return a rsndom letter from your name
+
+const nombre = "ochoa";
+console.log(typeof nombre);
+
+console.log(nombre.charAt((Math.floor(Math.random()*4)+1)));
+
+
+
+console.log(Math.floor(Math.random()*4)+1);
+console.log(Math.floor(Math.random()*4)+1);
+console.log(Math.floor(Math.random()*4)+1);
+console.log(Math.floor(Math.random()*4)+1);
+console.log(Math.floor(Math.random()*4)+1);
+
