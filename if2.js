4f120bbda7d33718284eb315a6bf3ce0352c5873
8A <32315516+Irlanda3@users.noreply.github.com>
2022-09-22 22:58:33 -0600

    Add files via upload

---
+++ if2.js
@@ -0,0 +1,12 @@
+let customerIsBanned = false;
+let soup = "chicken noodle soup";//the value of soup
+let reply;// undefined variable
+let crackers = true;
+
+if(customerIsBanned){
+    reply = "no sopa";
+} else if(soup && crackers){
+    reply = "here is your order";
+}
+console.log(reply);
+
