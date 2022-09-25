255f781d2d4b2ca50f03c97af2d7c2bee4ea204f
8A <32315516+Irlanda3@users.noreply.github.com>
2022-09-25 16:25:16 -0600

    Add files via upload
    switch case

---
+++ switch2.js
@@ -0,0 +1,16 @@
+switch("2"){
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);   
+        break;
+    case 2:
+        console.log(3);
+        break;
+
+    default:
+        console.log("No math");
+}
+
+// no match in this case bcaause of the data types
