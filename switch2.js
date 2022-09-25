7e635b358c9fe1a14df85f191d2a12869601b65e
8A <32315516+Irlanda3@users.noreply.github.com>
2022-09-25 16:46:06 -0600

    Add files via upload
    game added

--- switch2.js
+++ switch2.js
@@ -1,4 +1,6 @@
-switch("2"){
+
+//generate a random numbre from 1 to 3
+switch(Math.floor(Math.random() * 3 + 1)) {
     case 1:
         console.log(1);
         break;
@@ -10,7 +12,56 @@ switch("2"){
         break;
 
     default:
-        console.log("No math");
+        console.log("No match");
 }
 
 // no match in this case bcaause of the data types
+
+
+
+//Game
+
+let playerOne = "rock";
+let computer = "rock";
+
+switch (playerOne) {
+    case computer:
+        console.log("tie game!");
+        break;
+    case "rock":
+        if (computer === "paper"){
+            console.log("computer wins!");
+
+        }else{
+            console.log("playerOne wins");
+        }
+        break;
+
+    case "rock":
+        if (computer === "paper"){
+            console.log("computer wins!");
+
+        }else{
+            console.log("playerOne wins");
+        }
+        break;
+
+    case "paper":
+        if (computer === "scissors"){
+            console.log("computer wins!");
+
+        }else{
+            console.log("playerOne wins");
+        }
+        break;
+
+    defaut:
+        if (computer === "rock"){
+            console.log("computer wins!");
+
+        }else{
+            console.log("playerOne wins");
+        }
+        break;
+
+}
