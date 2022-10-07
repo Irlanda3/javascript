b14ab82db9b638e9777acf2668ab3aa11f54aa70
8A <32315516+Irlanda3@users.noreply.github.com>
2022-10-06 22:39:00 -0600

    Add files via upload

---
+++ arrayunshift.js
@@ -0,0 +1,7 @@
+const  fruteria =  ['banana','melon','tuna','naranja'];
+console.log(fruteria);
+
+const newfruteria = fruteria.unshift('papas');
+console.log(newfruteria); // returns 5
+
+console.log(fruteria);
