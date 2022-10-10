f63ef75318cb89c4471d80c30380bceee3546829
8A <32315516+Irlanda3@users.noreply.github.com>
2022-10-09 20:38:17 -0600

    Add files via upload
    new functions

--- function.js
+++ function.js
@@ -15,3 +15,36 @@ function suma(num1, num2){
 }
 
 console.log(suma(2,5));
+
+function getUserNameFromEmail(email) {
+    return email.slice(0, email.indexOf("@"));
+}
+
+console.log(getUserNameFromEmail("correo@gmail.com"));
+
+// Anonimus functions. we need a name
+const fnc = function(email) {
+    return email.slice(0, email.indexOf("@"));
+}
+
+console.log(fnc("correo@gmail.com"));
+
+
+//arrow functions
+
+const flechafnc = (email) => {
+    return email.slice(0, email.indexOf("@"));
+};
+
+console.log(fnc("correo@gmail.com"));
+
+
+//another example
+
+const toProperCase = (nombre) => {
+    return nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase();
+}
+console.log(toProperCase("dAvE"));
+
+
+
