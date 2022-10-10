6e71957262348fb3e9bd3e5c74516f7875245ae5
8A <32315516+Irlanda3@users.noreply.github.com>
2022-10-10 13:02:14 -0600

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,24 @@
+// objects
+// key-value pairs in curly braces
+const vehicle = {
+    wheels: 4,
+    engine: function () {
+        return "Vroom!";
+    }
+};
+
+
+const truck = Object.create(vehicle);
+truck.doors = 2;
+console.log(truck);
+console.log(truck.wheels);//inheritance
+console.log(truck.engine());
+const car =  Object.create(vehicle);
+car.doors = 4;
+car.engine = function() {
+    return 'ruuuun';// kind of overwrites the vroom funtion
+};
+console.log(car.engine());
+console.log(truck);
+
+console.log(vehicle);
