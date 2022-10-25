215b83cd8011dbdbf70887f2140abd5d9b097890
8A <32315516+Irlanda3@users.noreply.github.com>
2022-10-24 22:15:41 -0600

    Add files via upload

---
+++ santa.js
@@ -0,0 +1,20 @@
+function add(agregando) {
+    const participantes = [];
+    participantes.push(agregando);
+    console.log(participantes);
+    console.log(participantes.length);
+}
+
+// we use push to add to an array so we do not have holes
+// push adds to the end of the array
+
+ add("alma");
+ add("cuyo");
+
+
+ // now we to add tot he array by user input
+ //let person = Window.prompt("message");
+ //console.log(add(person));
+
+
+ // when we get all participants then ramdonly assign each other.
