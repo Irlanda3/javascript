13512ce7bd58c3ec85870a16e0979171ecd59ca6
8A <32315516+Irlanda3@users.noreply.github.com>
2022-10-30 21:54:28 -0600

    Add files via upload

--- challenge1.js
+++ challenge1.js
@@ -13,3 +13,9 @@ console.log(Math.floor(Math.random()*4)+1);
 console.log(Math.floor(Math.random()*4)+1);
 console.log(Math.floor(Math.random()*4)+1);
 
+//segun el tutorial empieza en zero el random number is le quitamos el +1
+console.log(Math.floor(Math.random()*4));
+console.log(Math.floor(Math.random()*4));
+console.log(Math.floor(Math.random()*4));
+console.log(Math.floor(Math.random()*4));
+console.log(Math.floor(Math.random()*4));
