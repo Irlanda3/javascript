63542b5a3677b7a66f2d96693f5f3baca2fe69fa
8A <32315516+Irlanda3@users.noreply.github.com>
2022-11-06 17:58:48 -0700

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,3 +1,5 @@
-fetch('https://regres.in/api/users')
- .then(response => console.log(response.json()))
- 
+import fetch from "node-fetch";
+console.log("hola");
+fetch("https://regres.in/api/users").then((response) =>
+  console.log(response.json())
+);
