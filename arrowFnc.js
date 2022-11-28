4699d4b8ab945d6b1d16c8f2db1a46728f39dc31
8A <32315516+Irlanda3@users.noreply.github.com>
2022-11-27 22:00:45 -0700

    Add files via upload
    CodingTube

--- arrowFnc.js
+++ arrowFnc.js
@@ -1,15 +1,27 @@
-//Arrow functions were introduced in ES6.
-
-//Arrow functions allow us to write shorter function syntax:
-
-let myFunction = (a, b) => a * b;
-
-hello = function() {
-    return "Hello World!";
-  }
-
-hello2 = () => {
-    return "Hello World!";
+//FUNCION CLASICA - HOISTING
+function saludar(nombre) {
+  return "hola " + nombre;
 }
 
-console.log("Hello");
+console.log(saludar("David"));
+
+//HOISTING: UNA FUNCION LA PUEDO LLAMAR DESDE CUAL QUIER PARTE DEL CODIGO
+//PUEDO INCLUSO LLAMARLA DESDE LA LINEA 1
+
+//FUNCION ANONIMA
+// EN ESTE CASO EL HOISTING NO SE PUEDE HACER. SE TIENE QUE LLAMAR DESPUES DE DECLARAR
+
+let saludo = function (nombre) {
+  return "Saludos " + nombre;
+};
+console.log(saludo("David"));
+
+// FUNCIONES FLECHA () => {}
+let saludos2 = (nombre) => {
+  "Saludos " + nombre;
+};
+//OR
+let salu2 = (nombre) => "saludos " + nombre;
+
+let suma = (a, b) => a + b;
+//https://www.youtube.com/watch?v=HVEkbCZAuqA
