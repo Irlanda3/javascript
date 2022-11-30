f3cf1c7a79578b1f48cc00238f85890fb89851e5
8A <32315516+Irlanda3@users.noreply.github.com>
2022-11-29 23:28:12 -0700

    Add files via upload

---
+++ objectsCodingTube.js
@@ -0,0 +1,14 @@
+// OBJETO
+let frutas = {
+  familias: "citricos",
+  cantidad: 200,
+  tipo: ["limon", "naranja", "mandarina"],
+};
+
+console.log(frutas);
+console.log(frutas.familias);
+console.log(frutas.cantidad);
+console.log(frutas.tipo);
+
+// AGREGAR NUEVA PROPIEDAD
+frutas.color = "rojo";
