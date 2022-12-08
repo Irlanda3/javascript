b9778e62cf9ef02ec5a8afa785a6394543102bee
8A <32315516+Irlanda3@users.noreply.github.com>
2022-12-07 22:30:35 -0700

    Add files via upload

---
+++ minMaxSum.js
@@ -0,0 +1,11 @@
+let numeros = 0;
+function miniMaxsum(arr) {
+  const arrayNums = [1, 3, 5, 7, 9];
+  for (let nums of arrayNums) {
+    numeros += nums;
+    console.log(numeros);
+  }
+}
+
+miniMaxsum();
+//solo queremos sumar 4 numeros
