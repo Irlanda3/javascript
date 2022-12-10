733085de5b21f4de745c25689f758e4dfa783577
8A <32315516+Irlanda3@users.noreply.github.com>
2022-12-09 23:45:57 -0700

    Add files via upload

---
+++ minmaxsuma.js
@@ -0,0 +1,27 @@
+let numeros = 0;
+
+function miniMaxsum(arr) {
+  const arrayNums = [1, 3, 5, 7, 9];
+
+  for (let nums of arrayNums) {
+    console.log(nums, " is element of the array");
+    let index = arrayNums.indexOf(nums); //0 to 4
+    console.log("en el index numero ", index);
+
+    const copia = [...arrayNums];
+    copia.splice(index, 1, 0);
+    console.log("After slice method", copia);
+
+    numeros += nums; // suma todos los numeros en el array
+    console.log("numeros", numeros);
+    const arrayCopy = [...arrayNums]; // copy array in ES6
+    let count = 0;
+    for (let elements of copia) {
+      count += elements;
+      console.log(count);
+    }
+
+    console.log("");
+  }
+}
+miniMaxsum();
