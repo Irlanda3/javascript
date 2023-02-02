70765df7d580ca2bf6ee91a93135cbc2be038c69
8A <32315516+Irlanda3@users.noreply.github.com>
2023-02-01 22:34:52 -0700

    Add files via upload
    changes

--- arrayquery.js
+++ arrayquery.js
@@ -5,12 +5,23 @@ const queries = ["ab", "abc", "bc"];
 //it occurs in the list of input strings.
 // In this case, there are 2 instances od 'ab', 1 of abc and 0 of bc
 // results = [2,1,0]
-
+countab = 1;
+countabc = 1;
+countbc = 1;
 queries.forEach(myFunction);
 
 function myFunction(items) {
-  console.log(items);
+  //console.log(items);
   for (const i in string) {
-    console.log(string[i], "string");
+    //console.log(string[i], "string");
+    //if (items === string[i]) {
+    //console.log("inside de if", count++);
+    if (items === "ab") {
+      console.log("found ab similar ab", countab++);
+    } else if (items === "abc") {
+      console.log("found abc similar abc");
+    } else if (items === "bc") {
+      console.log("found bc similar bc");
+    }
   }
 }
