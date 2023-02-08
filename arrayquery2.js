a43b11d9f60241a1ce188218706f2809f176d431
8A <32315516+Irlanda3@users.noreply.github.com>
2023-02-07 23:24:38 -0700

    Add files via upload

--- arrayquery2.js
+++ arrayquery2.js
@@ -1,6 +1,6 @@
 const stringArr = ["ab", "ab", "abc"];
-const queries = ["ab", "abc", "bc"];
 const iterator = stringArr.values();
+const queries = ["ab", "abc", "bc"];
 
 //  for each query string, determine how many times
 //it occurs in the list of input strings.
@@ -10,19 +10,24 @@ count = 1;
 queries.forEach(myFunction);
 
 function myFunction(items) {
-  console.log(items); // imprime  primero ab
+  console.log(">>>>>", items); // imprime  primero ab
   //console.log(typeof items);
   //    QUEREMOS COMPARAR AB CON EL ARRAY DE STRINGS
 
   for (const value of iterator) {
     // cual es la diff entre in y of en el for loop??
-    console.log(value);
+    console.log("printing value:  ", value);
+    console.log(items, "    ", value);
     if (items === value) {
       //ab==="ab"?, ab==="ab"?, ab==="ab?
       //console.log(count++);
-      console.log("kechos");
+      console.log("if  ", items, "  ===  ", value, " YES");
+      console.log();
+    } else {
+      console.log("NOT equal", items, " AND ", value);
     }
+    console.log("WHAT IS THIS FOR");
   }
-
-  //console.log("======== fuera del for loop ==");
 }
+
+// ABC=AB?  ABC=AB?   ABC=ABC?
