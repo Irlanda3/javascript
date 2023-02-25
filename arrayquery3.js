59b7387ffe571592a1478020440b94af1c14af34
8A <32315516+Irlanda3@users.noreply.github.com>
2023-02-24 23:58:44 -0700

    Add files via upload

--- arrayquery3.js
+++ arrayquery3.js
@@ -2,6 +2,7 @@ const stringArr = ["ab", "ab", "abc"];
 //const iterator = stringArr.values();
 const queries = ["ab", "abc", "bc"];
 
+let count = 0;
 queries.forEach(myFunction);
 
 function myFunction(items) {
@@ -11,6 +12,9 @@ function myFunction(items) {
   //console.log(value);
   //}
   for (let valor of stringArr) {
-    console.log("printing");
+    console.log("printing", valor);
+    if (items === valor) {
+      console.log("counting", count++); // este count me cuanta en general todas las repeticion
+    }
   }
 }
