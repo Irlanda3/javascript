db16cad70a71013b44433d00b39699c0d54fb69e
8A <32315516+Irlanda3@users.noreply.github.com>
2023-03-31 05:27:02 -0600

    Create Destructuring4.js

---
+++ Destructuring4.js
@@ -0,0 +1,21 @@
+/**
+Exercise 1
+Rewrite the code below to use array destructuring instead of assigning each value to a variable.
+*/
+{
+  console.log("EXERCISE 1");
+
+  let item = ["Egg", 0.25, 12];
+
+  let name = item[0];
+  let price = item[1];
+  let quantity = item[2];
+
+  console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+  console.log();
+}
+
+/**
+ Solveved
+const [name, price, quantity] = item;
+Console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
