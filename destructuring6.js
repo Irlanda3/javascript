c0252408bd91871d9d4a2b0e12ba375ee00b6e07
8A <32315516+Irlanda3@users.noreply.github.com>
2023-05-06 23:04:02 -0600

    Update destructuring6.js
    get the last name from the array

--- destructuring6.js
+++ destructuring6.js
@@ -1,1 +1,23 @@
+Exercise 6
+Using Array Destructuring get the last name from the array. 
+Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
+*/
+{
+  console.log("EXERCISE 6");
 
+  const students = ['Christina', 'Jon', 'Alexandare'];
+
+  // Write your code here
+  const [] = students;
+
+  console.log(lastName);
+  console.log();
+}
+
+/**
+
+Answer
+
+const students = ['Christina', 'Jon', 'Alexandare'];
+const [,,lastName] =  students;
+console.log(lastName);
