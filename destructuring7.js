0a3f88f6067ec2b29dc45abdde8299ebaf7d03f2
8A <32315516+Irlanda3@users.noreply.github.com>
2023-05-08 22:44:42 -0600

    Create destructuring7.js

---
+++ destructuring7.js
@@ -0,0 +1,19 @@
+Exercise 7
+Using Array Destructuring get all of the names from this Nested Array
+Hint: https://untangled.io/in-depth-es6-destructuring-with-assembled-avengers
+*/
+{
+  console.log("EXERCISE 7");
+
+  const moreStudents = [
+    'Chris', 
+    ['Ahmad', 'Antigoni'], 
+    ['Toby', 'Sam']
+  ];
+
+  // Write your code here
+  const [student1, [student2, student3], [student4, student5]] = moreStudents;
+
+  console.log(student1, student2, student3, student4, student5);
+  console.log();
+}
