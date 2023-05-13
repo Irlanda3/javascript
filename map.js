5857161429b2f9e2ab173c71645c0a8b74a3cdc8
8A <32315516+Irlanda3@users.noreply.github.com>
2023-05-12 23:27:46 -0600

    Create map.js
    chatgpt ex

---
+++ map.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+
+// double each number
+const doubledNumbers = numbers.map(function(number) {
+  return number * 2;
+});
+
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
